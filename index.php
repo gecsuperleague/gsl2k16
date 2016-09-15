a715eb477e2a8b032eb7d2f4dac3ec04ffc93040
gecsuperleague <gecsuperleague@gmail.com>
2016-09-15 15:08:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+   ?>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>GSL 2k16
+  </title>
+	<link rel="shortcut icon" href="images/gslicon.ico" type="image/x-icon" >
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/owl.carousel.css">
+
+  <!-- Google Fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet">
+
+  <!--[if IE 9]>
+    <script src="js/media.match.min.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<div id="main-wrapper">
+
+  <!-- Start Header -->
+  <header id="header" class="second-version">
+    <div class="container">
+      <div class="header-logo">
+        <a href="index.html"><img src="images/logoq.png" alt=""></a>
+        <div class="triangle-left"></div>
+        <div class="triangle-right"></div>
+      </div>
+    </div>
+    <div class="header-navbar">
+      <div class="container">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <nav class="navigation">
+            <ul class="list-inline text-center custom-list">
+				  <li><a href="#home"><i class="fa fa-home"></i> Home</a></li>				  
+				  <li><a href="#register"><i class="fa fa-level-up"></i> Register</a></li>
+				  <li><a href="#news"><i class="fa fa-list-alt"></i> News</a></li>
+				  <li><a href="#teams"><i class="fa fa-group"></i> Teams</a></li>
+				  <!--
+				  <li><a href="#managers"><i class="fa fa-user"></i> Managers</a></li>
+				  <li><a href="#players"><i class="fa fa-th-list"></i> Players</a></li>-->
+				  
+				  <li><a href="#social"><i class="fa fa-comment"></i> Contact Us</a></li>
+				  <!--<li><a href="#about"><i class="fa fa-futbol-o"></i> Matches</a></li>
+				  <li><a href="#fixtures"><i class="fa fa-calendar"></i> Fixtures</a></li>				  
+				  <li><a href="#results"><i class="fa fa-th-large"></i> Results</a></li>
+				  <li><a href="#social"><i class="fa fa-arrow-up"></i> Leader Board</a></li>-->
+             
+            </ul>
+          </nav>
+          <ul class="social list-inline">
+            <li><a href="https://www.facebook.com/GSLgecw" target="_blank"><i class="fa fa-facebook"></i></a></li>        
+          </ul>
+          <button class="navbar-toggle">
+            <i class="fa fa-list"></i>          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- End Header -->
+  
+  <!-- Start Intro -->
+  <div class="intro" id="home">
+    <div id="home-slider" class="fixed-nav owl-carousel owl-theme">
+      <div class="item">
+        
+        <!-- Start Video Background 
+        <div id="video_intro" data-vide-bg="video/soccer" data-vide-options="position: 0% 50%"></div>
+         <div class="video_gradient"></div>-->
+        <!-- End Video Background -->
+		<img src="img/slider/bg5.jpg" alt="">
+		<div class="video_gradient"></div>
+        <div class="slide-content col-lg-6 col-lg-offset-3 col-md-12 col-sm-12 text-center">
+		 <div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>WELCOME</p>
+				   <h2>GEC <span class="span">SUPER</span> LEAGUE</h2>
+				   <h4>BE A <span class="span">MANAGER</span> <br>OR<br> BE A <span class="span">PLAYER</span></h4>
+					<a href="#register" class="button"><span>Register Now</span><i class="fa fa-arrow-circle-right"></i></a>
+				</div>
+		 </div>
+	
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/slider/bg8.jpg" alt="">
+		<div class="video_gradient"></div>
+        <div class="slide-content col-lg-8 col-lg-offset-2 col-md-12 col-sm-12 text-center">
+		 <h2>" WHAT I DO IS PLAY SOCCER<BR>WHICH IS WHAT I LIKE "</h2>
+          <p>- LIONEL MESSI</p>
+		   <h3>LET'S FOOTBALL GEC</h3>
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/slider/bg7.jpg" alt="">
+		<div class="video_gradient"></div>
+        <div class="slide-content col-lg-8 col-lg-offset-2 col-md-12 col-sm-12 text-center">
+          <h2>" WITHOUT FOOTBALL<BR>MY LIFE IS WORTH NOTHING "</h2>
+          <p>- CRISTIANO RONALDO</p>
+		   <h3>LET'S FOOTBALL GEC</h3>
+			
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/slider/bg9.jpg" alt="">
+		<div class="video_gradient"></div>
+        <div class="slide-content col-lg-8 col-lg-offset-2 col-md-12 col-sm-12 text-center">
+         <h2>" IF IT DOESN'T CHALLENGE YOU<BR>IT DOESN'T CHANGE YOU "</h2>
+		   <h3>LET'S FOOTBALL GEC</h3> 
+        </div>
+      </div>
+    </div>
+    <div class="customNavigation">
+      <a class="btn prev"><i class="fa fa-angle-left"></i></a>
+      <a class="btn next"><i class="fa fa-angle-right"></i></a>
+    </div>
+  </div>
+  <!-- End Intro -->
+  
+  <div id="register">
+    <div class="container" >
+      <!-- Start Table -->
+        <h4 class="border">REGISTER NOW</h4>
+		<div class="row">
+			<div class="col-md-6 register">
+				<h3>Want to be a manager?</h3>
+				<p>A good manager is one who takes interest in understanding, developing and building his team.
+				Build your team, take charge of your own team, buy and sell players, train your players and start a new season. 
+				Let's join now
+				</p>
+				
+				<a href="#" class="button"><span>Register Now</span><i class="fa fa-arrow-circle-right"></i></a>
+			</div>
+			<div class="col-md-6 register">
+				<h3>Want to be a player?</h3>
+				<p>Get hired, Sign your cotnracts, Be the part of a good team, Train yourself, Play for your team and Start a new season. Let's join now</p>
+				<a href="#" class="button"><span>Register Now</span><i class="fa fa-arrow-circle-right"></i></a>
+
+			</div>
+		</div>
+	</div>
+  </div>
+  <!-- End Social -->  
+	<!-- Start News -->
+  <div id="news" class="news">
+    <div class="container">
+
+      <!-- Start Heading -->
+      <div class="heading col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h4 class="border">Latest News</h4>
+       <!-- <a href="#" class="rest pull-right"><span>See all news</span><i class="fa fa-arrow-circle-right"></i></a>-->
+      </div>
+      <!-- End Heading -->
+      
+      <!-- Start Single-News -->
+      <div class="single-news col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <article>
+          <img src="img/news.png" alt="" class="thumbnail">
+          <div class="article-content">
+            <h5><a href="#">Registration Have Been Opened:</a></h5>
+            <p>The registration for Managers and Players have been opened. Links are available in the website </p>
+          </div>
+        </article>
+      </div>
+      <!-- End Single-News -->
+
+      <!-- Start Single-News -->
+      <div class="single-news col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <article>
+          <img src="img/news.png" alt="" class="thumbnail">
+          <div class="article-content">
+            <h5><a href="#">Last Date for Registration:</a></h5>
+            <p>The last Date for Registration will be announced soon</p>
+          </div>
+        </article>
+      </div>
+      <!-- End Single-News -->
+
+      <!-- Start Single-News -->
+      <div class="single-news col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <article>
+          <img src="img/news.png" alt="" class="thumbnail">
+          <div class="article-content">
+            <h5><a href="#">List of Managers and Players:</a></h5>
+            <p>The list of registered Managers and Players will be updated soon</p>
+          </div>
+        </article>
+      </div>
+      <!-- End Single-News -->
+      
+    </div>
+  </div>
+  <!-- End News -->
+  
+  <!-- Start Teams -->
+  <div id="teams" class="teams">
+    <div class="container">
+
+      <!-- Start Heading -->
+      <div class="heading col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h4 class="border">Our Teams</h4>
+      </div>
+      <!-- End Heading -->
+      
+      <!-- Start Teams-Frame -->
+      <div class="teams-frame col-lg-12">  
+        <!-- Start Tab Seniors -->
+        <div class="tab-pane fade active in" id="senior-team">
+          <!-- Start Player-Profile -->
+          <div class="player-profile">
+            
+            <!-- Start Profiles-Slider -->
+            <div class="profile-slider">                         					   			
+			   <!-- Start Profile-Single -->
+              <div class="profile-single">
+                <div class="big-thumbnail">
+                  <img src="images/team.png" alt="">
+                </div>
+                <div class="player-description">
+                
+                  <h3>Team details will be updated soon...</h3>
+                <!--  <img src="img/player-profile-logo.png" alt="" class="logo-background">
+				<ul class="player-details pull-left custom-list">
+                   <li><h5>Manager:</h5><span>Full Name</span></li>
+				   <li><h5>Captain:</h5><span>Full Name</span></li>    
+                                    
+                  </ul>
+                  <ul class="player-details pull-right custom-list">
+                    <li><h5>Formation:</h5><span>4-4-3</span></li>
+                     <li><h5>Phone:</h5><span>6667772343</span></li>                
+                  </ul>
+				  <ul class="player-details pull-left custom-list">
+                    <li><h5>1</h5><span>Full Name</span></li>
+					<li><h5>2</h5><span>Full Name</span></li>
+					<li><h5>3</h5><span>Full Name</span></li>
+					<li><h5>4</h5><span>Full Name</span></li>  
+					<li><h5>5</h5><span>Full Name</span></li>
+                  </ul>
+				  <ul class="player-details pull-right custom-list">
+                    
+					<li><h5>6</h5><span>Full Name</span></li>
+					<li><h5>7</h5><span>Full Name</span></li>
+					<li><h5>8</h5><span>Full Name</span></li> 
+					<li><h5>9</h5><span>Full Name</span></li>
+                  </ul>
+<!--                  <a href="#" class="button read_more"><span>Read More</span><i class="fa fa-arrow-circle-right"></i></a>
+--> 
+                 </div>
+              </div>
+              <!-- End Profile-Single -->
+            </div>
+            <!-- End Profiles-Slider -->
+			
+
+          </div>
+          <!-- End Player-Profile -->
+      </div>
+      <!-- End Teams-Frame -->
+
+    </div>
+  </div>
+  <!-- End Teams -->
+ </div>
+ 
+  <!-- Start Social -->
+  <div id="social">
+    <div class="container">
+      
+      <!-- Start Heading -->
+      <div class="heading col-lg-12 col-md-12 col-sm-12">
+        <h4 class="border">
+          Contact Us
+        </h4>
+      </div>
+      <!-- End Heading -->
+
+      <div class="customNavigation">
+        <a class="btn prev"><i class="fa fa-angle-left"></i></a>
+        <a class="btn next"><i class="fa fa-angle-right"></i></a>
+      </div>
+      
+      <!-- Start Testimonials -->
+      <div class="testimonials-slider col-lg-12 col-md-12 col-sm-12">
+        <div class="testimonial left">
+          <div class="thumbnail">
+            <img src="img/user1.png" alt="">
+          </div>
+          <blockquote>
+            <h4 class="author">Vivek KV</h4>
+            <span class="data">S7 EEE</span>
+             <i class="fa fa-phone"> : (+91) 81290 44472</i><br>
+			 <i class="fa fa-envelope"> : gecsuperleague@gmail.com</i>
+          </blockquote>
+        </div>
+        <div class="testimonial right">
+          <div class="thumbnail">
+            <img src="img/user1.png" alt="">
+          </div>
+          <blockquote>
+            <h4 class="author">Nibin</h4>
+            <span class="data">S3 EC1</span>
+             <i class="fa fa-phone"> : (+91) 86067 35579</i><br>
+			<i class="fa fa-envelope"> : gecsuperleague@gmail.com</i>
+          </blockquote>
+        </div> 
+      </div>
+      <!-- End Testimonials -->
+
+    </div>
+  </div>
+  <!-- End Social -->
+  <!-- Start Footer -->
+  <footer id="footer" class="row">
+     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">&copy; 2016 Zesc Solutions| All Rights Reserved.</div>
+	 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12"># AIT</div>
+	<a href="#" id="back-to-top">
+    <i class="fa fa-angle-up"></i>
+   </a>
+  </footer>
+  <!-- End Footer -->
+</div> 
+<!-- end #main-wrapper -->
+
+<!-- Scripts -->
+<script src="js/jquery.min.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.ba-outside-events.min.js"></script>
+<script src="js/tab.js"></script>
+<script src='js/bootstrap-datepicker.js'></script>
+<script src="js/jquery.vide.min.js"></script>
+<script src="twitter/jquery.tweet.min.js" type="text/javascript"></script>
+
+</body>
+
+</html>
